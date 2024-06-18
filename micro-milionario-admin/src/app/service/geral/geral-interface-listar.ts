32759dfc0f3b0a002fbb544aa8cfbcb0ef715de7
Roshnim123 <roshni.m@cisinlabs.com>
2024-06-18 11:10:53 +0530

    latest code

--- micro-milionario-admin/src/app/service/geral/geral-interface-listar.ts
+++ micro-milionario-admin/src/app/service/geral/geral-interface-listar.ts
@@ -1,11 +1,36 @@
 export interface GeralInterfaceListar {
+  
     code: number,
     message: string,
     dados:{
         total:number,
         perPage: number,
         page: number,
-       lastPage: number,
-       data: []
+        lastPage: number,
+        data: []
     }
 }
+
+
+export interface GeralInterfacePousar {
+    code: number,
+    message: string,
+    dados:{
+    created_at: string
+    description: string
+    file: string
+    id: number
+    title: string
+    type: string
+    updated_at: string
+    }
+  }
+
+
+  export interface GeralInterfaceImages {
+    id: number;
+    name: string;
+    url: string;
+    code: string;
+    description: string;
+  }
