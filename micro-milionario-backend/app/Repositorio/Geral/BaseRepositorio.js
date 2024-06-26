1ddb46bd55ac5229100daaf559420a0e66021c6b
Prashant <prashant.p@cisinlabs.com>
2024-06-26 15:19:34 +0530

    formatted code

--- micro-milionario-backend/app/Repositorio/Geral/BaseRepositorio.js
+++ micro-milionario-backend/app/Repositorio/Geral/BaseRepositorio.js
@@ -1,182 +1,142 @@
-const NotFoundException = use('App/Exceptions/NotFoundException')
-const FailCreationException = use('App/Exceptions/FailCreationException')
+const NotFoundException = use("App/Exceptions/NotFoundException");
+const FailCreationException = use("App/Exceptions/FailCreationException");
 class BaseRepositorio {
-    constructor(model) {
-        this.model = model
-        this.Model = use(`App/Models/${this.model}`)
+  constructor(model) {
+    this.model = model;
+    this.Model = use(`App/Models/${this.model}`);
+  }
+
+  async findById(modelId) {
+    const modelResponse = await this.Model.query()
+      .where("id", modelId)
+      .where("is_delete", false)
+      .first();
+
+    if (!modelResponse) {
+      throw new NotFoundException();
     }
 
+    return modelResponse.toJSON();
+  }
 
-    async findById(modelId) {
+  async findById1(modelId) {
+    const modelResponse = await this.Model.query().where("id", modelId).first();
 
-        const modelResponse = await this.Model
-            .query()
-            .where('id', modelId)
-            .where('is_delete', false)
-            .first()
-
-        if (!modelResponse) {
-            throw new NotFoundException;
-        }
-
-        return modelResponse.toJSON()
-
+    if (!modelResponse) {
+      throw new NotFoundException();
     }
 
-    async findById1(modelId) {
+    return modelResponse.toJSON();
+  }
 
-        const modelResponse = await this.Model
-            .query()
-            .where('id', modelId)
-            .first()
+  async findLastId() {
+    const modelResponse = await this.Model.max("id").fetch();
 
-        if (!modelResponse) {
-            throw new NotFoundException;
-        }
-
-        return modelResponse.toJSON()
-
+    if (!modelResponse) {
+      throw new NotFoundException();
     }
 
-    async findLastId() {
-        const modelResponse = await this.Model
-            .max('id')
-            .fetch();
+    return modelResponse.toJSON();
+  }
 
-        if (!modelResponse) {
-            throw new NotFoundException;
-        }
-
-        return modelResponse.toJSON()
+  async create(Data) {
+    try {
+      const criar = await this.Model.create(Data);
+      return criar.toJSON();
+    } catch (error) {
+      console.log(error);
+      throw new FailCreationException();
     }
+  }
 
-    async create(Data) {
-        try {
-            const criar = await this.Model.create(Data)
-            return criar.toJSON();
-        } catch (error) {
-            console.log(error);
-            throw new FailCreationException
-        }
-
+  async delete(id) {
+    try {
+      return await this.Model.query().where("id", id).update({ is_delete: 1 });
+    } catch (error) {
+      console.log(error);
     }
-
-    async delete(id) {
-        try {
-
-            return await this.Model.query().where('id', id).update({ is_delete: 1 })
-        } catch (error) {
-            console.log(error)
-
-        }
+  }
+  async update(id, dados) {
+    try {
+      await this.Model.query().where("id", id).update(dados);
+      // Fetch and return the updated data
+      const updatedData = await this.Model.find(id);
+      return updatedData;
+    } catch (error) {
+      console.log(error);
     }
-    async update(id, dados) {
+  }
 
-        try {
-            await this.Model.query().where('id', id).update(dados);
-            // Fetch and return the updated data
-            const updatedData = await this.Model.find(id);
-            return updatedData;
-        } catch (error) {
+  async showById(col, id) {
+    try {
+      return await this.Model.query()
+        .where(col, id)
+        .whereNot({ is_delete: true })
+        .fetch();
+    } catch (error) {}
+  }
 
-            console.log(error)
-
-        }
+  async findByCol(col, id) {
+    try {
+      return await this.Model.query().where(col, id).first();
+    } catch (error) {
+      console.log(error);
+      return;
     }
+  }
 
-    async showById(col, id) {
-        try {
-            return await this.Model.query()
-                .where(col, id)
-                .whereNot({ is_delete: true })
-                .fetch();
-        } catch (error) {
+  async showByIdAlternative(col, id) {
+    try {
+      return await this.Model.query().where(col, id).first();
+    } catch (error) {}
+  }
 
-        }
-    }
+  async showById1(col, id) {
+    try {
+      return await this.Model.query()
+        .where(col, id)
+        .whereNot({ is_active: true })
+        .whereNot({ is_delete: true })
+        .fetch();
+    } catch (error) {}
+  }
 
-    async findByCol(col, id) {
-        try {
-            return await this.Model.query()
-                .where(col, id)
-                .first();
-        } catch (error) {
-            console.log(error)
-            return;
-        }
-    }
+  async index() {
+    try {
+      return await this.Model.query().whereNot({ is_delete: true }).fetch();
+    } catch (error) {}
+  }
 
-    async showByIdAlternative(col, id) {
-        try {
-            return await this.Model.query()
-                .where(col, id)
-                .first();
-        } catch (error) {
+  async indexActive() {
+    try {
+      return await this.Model.query()
+        .whereNot({ is_active: false })
+        .whereNot({ is_delete: true })
+        .fetch();
+    } catch (error) {}
+  }
 
-        }
-    }
+  async indexPure() {
+    try {
+      return await this.Model.query()
+        .whereNot({ is_active: true })
+        .whereNot({ is_delete: true })
+        .fetch();
+    } catch (error) {}
+  }
 
-    async showById1(col, id) {
-        try {
-            return await this.Model.query()
-                .where(col, id)
-                .whereNot({ is_active: true })
-                .whereNot({ is_delete: true })
-                .fetch();
-        } catch (error) {
+  async find() {
+    try {
+      const allRecords = await this.Model.query().fetch();
+      return allRecords.toJSON();
+    } catch (error) {}
+  }
 
-        }
-    }
-
-    async index() {
-        try {
-            return await this.Model.query()
-                .whereNot({ is_delete: true })
-                .fetch();
-        } catch (error) {
-
-        }
-    }
-
-    async indexActive() {
-        try {
-            return await this.Model.query()
-                .whereNot({ is_active: false })
-                .whereNot({ is_delete: true })
-                .fetch();
-        } catch (error) {
-
-        }
-    }
-
-    async indexPure() {
-        try {
-            return await this.Model.query()
-                .whereNot({ is_active: true })
-                .whereNot({ is_delete: true })
-                .fetch();
-        } catch (error) {
-
-        }
-    }
-
-    async find() {
-        try {
-            const allRecords = await this.Model.query().fetch();
-            return allRecords.toJSON();
-        } catch (error) {
-
-        }
-    }
-
-    async deleteData(col, id) {
-        try {
-            return await this.Model.query()
-                .where(col, id).delete()
-        } catch (error) {
-
-        }
-    }
+  async deleteData(col, id) {
+    try {
+      return await this.Model.query().where(col, id).delete();
+    } catch (error) {}
+  }
 }
 
 module.exports = BaseRepositorio;
