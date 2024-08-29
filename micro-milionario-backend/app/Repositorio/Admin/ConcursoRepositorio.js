8faeddf3e8f31e36e1093be8ef61f09d90713fef
Prashant <prashant.p@cisinlabs.com>
2024-08-29 15:09:11 +0530

    removed is_active checking

--- micro-milionario-backend/app/Repositorio/Admin/ConcursoRepositorio.js
+++ micro-milionario-backend/app/Repositorio/Admin/ConcursoRepositorio.js
@@ -11,7 +11,7 @@ class ConcursoRepositorio{
     async cadastrado(dados) {
 
         const concurso = await ConcursoModel.query()
-            .whereNot({ is_active: true }) // Se = 0
+            // .whereNot({ is_active: true }) // Se = 0
             .whereNot({ is_delete: true }) // Se = 0
             .where('nome', dados.nome)
             .getCount();
@@ -26,7 +26,7 @@ class ConcursoRepositorio{
         if(dados==undefined || dados== null){
             concursoListar= await ConcursoModel.query()
             .orderBy("created_at", 'desc')
-            .whereNot({ is_active: true }) // Se = 0 = Activo
+            // .whereNot({ is_active: true }) // Se = 0 = Activo
             .whereNot({ is_delete: true }) // Se = 0
             .paginate(pagination.page, pagination.perPage)
             
@@ -36,7 +36,7 @@ class ConcursoRepositorio{
             concursoListar= await ConcursoModel.query()
             .orderBy("created_at", 'desc')
             .where('nome', 'like', `%${dados}%`)
-            .whereNot({ is_active: true })
+            // .whereNot({ is_active: true })
             .whereNot({ is_delete: true })
             .paginate(pagination.page, pagination.perPage)
 
@@ -50,7 +50,7 @@ class ConcursoRepositorio{
       if(dados==undefined || dados== null){
           concursoListar= await ConcursoModel.query()
           .orderBy("created_at", 'desc')
-          .whereNot({ is_active: true })
+        //   .whereNot({ is_active: true })
           .whereNot({ is_delete: true })
           .fetch();
           // .paginate(pagination.page, pagination.perPage)
@@ -60,7 +60,7 @@ class ConcursoRepositorio{
           concursoListar= await ConcursoModel.query()
           .orderBy("created_at", 'desc')
           .where('nome', 'like', `%${dados}%`)
-          .whereNot({ is_active: true })
+        //   .whereNot({ is_active: true })
           .whereNot({ is_delete: true })
           .fetch();
           // .paginate(pagination.page, pagination.perPage)
